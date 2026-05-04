4d5a60c3ae60b7ad7b5dbc30bbc2fb2306e6b88f
ahmadkmhp-lang <devilkmhp@gmail.com>
2026-05-04 21:51:09 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 
+const PORT = process.env.PORT || 10000;
+
 app.get("/", (req, res) => {
-  res.send("Noor Notes AI running 🚀");
+  res.send("NoorNotesAi running 🚀");
 });
 
-app.listen(3000, () => {
-  console.log("Server running");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
