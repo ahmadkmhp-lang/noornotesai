f28834415fda3eb6ade682ca6b4bf9727bcbf2ea
ahmadkmhp-lang <devilkmhp@gmail.com>
2026-05-08 20:06:01 +0530

    fix public folder path

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const app = express();
 
 app.use(express.json());
 
-app.use(express.static(__dirname));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.post("/ai", async (req, res) => {
